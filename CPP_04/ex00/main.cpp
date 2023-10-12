a1577f446342cad6c4a9e4ec2fec01e44c155548
chesytoast <chesytoast@DESKTOP-F07108K>
2023-10-12 14:10:01 +0200

    wrongclass added

--- CPP_04/ex00/main.cpp
+++ CPP_04/ex00/main.cpp
@@ -1,17 +1,20 @@
 #include "Cat.hpp"
 #include "Dog.hpp"
-#include <stdlib.h>
+#include "WrongCat.hpp"
 
 int	main(){
 	Animal *a = new Cat();
 	Animal *b = new Dog();
 	Animal *c = new Animal();
+	WrongAnimal *w = new WrongCat();
 	
+	w->makeSound();
 	a->makeSound();
 	b->makeSound();
 	c->makeSound();
 	delete a;
 	delete b;
 	delete c;
+	delete w;
 	return 0;
 }
